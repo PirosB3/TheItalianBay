7337190d068d1b513b119dab83ef913d5f8d2a98
PirosB3 <pirosb3@gmail.com>
2011-06-02 17:31:08 +0200

    Added parsing to Pirate Bay API. It can currently parse search results!

--- PirateBayAPI.py
+++ PirateBayAPI.py
@@ -10,11 +10,47 @@ Written By PirosB3, http://pirosb3.com
 from google.appengine.api import urlfetch
 from urllib import quote
 
+from libs.BeautifulSoup import BeautifulSoup
+
 # TODO: Make order for, for now order by seed
 
 class PirateBayAPI():
     """This component is used to generate results for thepiratebay.org"""
     uri = 'http://thepiratebay.org'
+    
+    def __parseResult(self, result):
+        """Returns a list of results, each result has: name, link, SE, LE"""
+    	parser = BeautifulSoup(result)
+        results = []
+        
+    	# find results within table #searchResult and get a list of rows
+    	resultsTable = parser.find('table', {'id' : 'searchResult'})
+    	if resultsTable == None: return {}
+    	
+    	resultRows = resultsTable.findAll("tr")
+    	
+        # Iterate over each row and store results in list
+        if (len(resultRows) == 0 or resultRows == None): return {}
+    	for result in resultRows:
+    	    
+            # Get all defenitions in row and initialize empty dictionary
+    		elements = result.findAll('td')
+    		current = {}
+    		
+            # Iterate!
+    		for position, item in enumerate(elements):
+    			if position == 1:
+    				link = item.find("a", { "class" : "detLink" })
+    				current['title'] = link.text
+    				current['permalink'] = link['href']
+    			if position == 2:
+    				current['SE'] = item.text
+    			if position == 3:
+    				current['LE'] = item.text
+    		results.append(current)
+            # Validate iteration has been done correctly
+        
+        return results
     
     def requestResultsForValue(self, value):
         """return an array of results given a value as input"""
@@ -22,8 +58,7 @@ class PirateBayAPI():
         
         # Generate URI and make call to ThePirateBay
         call = "%s/search/%s/0/7/0" % (self.uri, quote(value))
-        result = urlfetch.fetch(call)
+        result = urlfetch.fetch(call).content
         
         # TODO: Parse URL
-        return result
-        
+        return self.__parseResult(result)
