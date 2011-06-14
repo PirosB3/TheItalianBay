8a196bb2176ce5b5c21ae326fed4812fba5d669f
PirosB3 <pirosb3@gmail.com>
2011-06-14 12:11:31 +0200

    Adding search handler

--- handlers/main.py
+++ handlers/main.py
@@ -4,15 +4,33 @@ from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 
 from settings import TEMPLATE_PATH
+from libs.PirateBayAPI import PirateBayAPI
 
 # GET /
 class RootHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write(template.render(TEMPLATE_PATH + 'index.html', {}))
 
+# GET /requestResultsForValue?value=spider+man&filter=video
+class RequestResultsForValueHandler(webapp.RequestHandler):
+    def get(self):
+        # Check if values have been passed, if not redirect to root
+        
+        value = self.request.get('value')
+        if value is '':
+            self.redirect('/')
+            
+        # Check if filters have been applied
+        filter = self.request.get('filter')
+        if filter is '':
+            filter = 'none'
+            
+        # Render Response
+        results = PirateBayAPI().requestResultsForValue(value=value, filter=filter)
+        self.response.out.write(results)
 
 def main():
-    application = webapp.WSGIApplication([('/', RootHandler)],
+    application = webapp.WSGIApplication([('/', RootHandler), ('/requestResultsForValue', RequestResultsForValueHandler)],
                                          debug=False)
     util.run_wsgi_app(application)
 
