9d8b4ceb16a9bb3b9ec54f3381fb5ff0f304e9df
PirosB3 <pirosb3@gmail.com>
2011-06-15 11:17:14 +0200

    Adding animated content. Created a cool header image!

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
 $(function(){
-
-});
+    $('.section-title').live('click', function()
+    {
+        $(this).parent().find('.section-content').slideToggle('slow');
+    });
+    
+    $('.startup-closed > .section-content').hide();
+});
