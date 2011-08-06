7df427953aaa4188a021f0f8f5d9b6e5a16f1ae6
PirosB3 <pirosb3@gmail.com>
2011-08-06 13:59:45 +0200

    added client-side script in order to post pretty urls from form

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,5 @@
 $(function(){
     
-    // Validate Search Field
-    $('#search-form').submit(function(e)
-    {
-        var x = $('search-field');
-        if (document.getElementById('search-field').value.length == 0)
-        {
-            e.preventDefault();
-        }
-    })
-    
     // For Sections
     $('.section-title').live('click', function()
     {
@@ -29,4 +19,35 @@ $(function(){
             $(this).removeClass('hover');
         }
     );
+    
+    // Create custom urls on form
+    $('#search-form').submit(function(e)
+    {
+        e.preventDefault();
+        
+        // if field is empty, just return
+        if (document.getElementById('search-field').value.length == 0) { return; }
+        
+        // start forming custom url
+        var value = document.getElementById('search-field').value
+        var filter = getFilter();
+        
+        // replace url with the constructed one
+        document.location = "http://theitalianbay.appspot.com/s/" + value + "/" + filter;
+    });
+    
+    // Helper function, returns the first checked filter in the search form
+    function getFilter()
+    {
+        filter = "";
+        
+        $('#filter-field > input[type=checkbox]').each(function(k,v)
+        {
+            if(v.checked)
+            {
+                filter = ("f/" + v.value + "/");
+            }
+        });
+        return filter;
+    }
 });
