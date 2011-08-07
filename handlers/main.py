61a8d193a18d51883f0d237c6c398e30d217a9c2
PirosB3 <pirosb3@gmail.com>
2011-08-07 14:07:46 +0200

    Revert 9e99ad1d0b4d519ef60f87bc93915a229d11406a^..HEAD

--- handlers/main.py
+++ handlers/main.py
@@ -6,8 +6,6 @@ from google.appengine.ext.webapp import template
 from settings import TEMPLATE_PATH
 from libs import PirateBayAPI
 
-from urllib import url2pathname
-
 # GET /
 class RootHandler(webapp.RequestHandler):
     def get(self):
@@ -17,35 +15,43 @@ class RootHandler(webapp.RequestHandler):
 
 # GET /requestResultsForValue?value=spider+man&filter=video
 class RequestResultsForValueHandler(webapp.RequestHandler):
-    def get(self, value, filter= 'none', orderby= 'SE'):
-                
-         # Check if values have been passed, if not redirect to root
-         if value == "":
-             return self.redirect('/')
-         
-         # Decode value parameter
-         value = url2pathname(value)
-         
-         # Render Response
-         results = PirateBayAPI.requestResultsForValue(value=value, filter=filter, orderBy=orderby)
-         base_url = self.request.host_url + '/s/%s/f/%s/' % (value, filter)
-         self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : "Results for: %s" % value, 'sortable' : True, 'base_url' : base_url}))
+    def get(self):
+        
+        # Check if values have been passed, if not redirect to root
+        value = self.request.get('value')
+        if value == "":
+            return self.redirect('/')
+            
+        # Check if filters have been applied
+        filter = self.request.get('filter')
+        if filter == '':
+            filter = 'none'
+            
+        orderBy = self.request.get('orderby')
+        if orderBy == '':
+            orderBy = 'SE'
+            
+        # Render Response
+        results = PirateBayAPI.requestResultsForValue(value=value, filter=filter, orderBy=orderBy)
+        base_url = self.request.path + '?value=%s&filter=%s' % (value, filter)
+        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'original_query' : value, 'sortable' : True, 'base_url' : base_url}))
 
 # GET /requestResultsforTop100?filter=video
 class RequestResultsforTop100(webapp.RequestHandler):
-    def get(self, filter= 'none'):
+    def get(self):
+        filter = self.request.get('filter')
+        if filter == '':
+            filter = 'none'
+        
         results = PirateBayAPI.requestResultsforTop100(filter=filter)
-        
-        title = "Top 100"
-        if filter != 'none': title += " in %s" % filter
-        
-        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : title, 'sortable' : False}))
+        base_url = self.request.path + '?filter=%s' % filter
+        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'original_query' : '', 'sortable' : False}))
 
 # GET /requestResultsforRecentUploads
 class RequestResultsforRecentUploads(webapp.RequestHandler):
     def get(self):
         results = PirateBayAPI.requestResultsforRecentUploads()
-        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : 'Recent uploads', 'sortable' : False}))
+        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'original_query' : '', 'sortable' : False}))
 
 # END TORRENT SEARCH HANDLERS
 
@@ -64,12 +70,8 @@ class RequestTorrentForResultURL(webapp.RequestHandler):
         self.response.out.write(fileURL)
 
 def main():
-    application = webapp.WSGIApplication([
-    ('/s/(.*)/f/(.*)/o/(.*)/', RequestResultsForValueHandler), ('/s/(.*)/f/(.*)/', RequestResultsForValueHandler), ('/s/(.*)/', RequestResultsForValueHandler),
-    ('/top/f/(.*)/', RequestResultsforTop100), ('/top/', RequestResultsforTop100),
-    ('/requestTorrentForResultURL', RequestTorrentForResultURL), ('/requestResultsforRecentUploads', RequestResultsforRecentUploads),
-    ('/', RootHandler)],
-    debug=False)
+    application = webapp.WSGIApplication([('/', RootHandler), ('/requestResultsForValue', RequestResultsForValueHandler), ('/requestResultsforTop100', RequestResultsforTop100), ('/requestTorrentForResultURL', RequestTorrentForResultURL), ('/requestResultsforRecentUploads', RequestResultsforRecentUploads)],
+                                         debug=False)
     util.run_wsgi_app(application)
 
 
