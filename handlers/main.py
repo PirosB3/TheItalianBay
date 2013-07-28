3176fe96efa5b49e6d619c2e51ba24853df7727c
Daniel Pyrathon <pirosb3@gmail.com>
2013-07-28 03:25:51 +0200

    updated to python 2.7 and reordered

--- handlers/main.py
+++ handlers/main.py
@@ -1,17 +1,25 @@
+import os
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
+import webapp2
+import jinja2
 
 from settings import TEMPLATE_PATH
 from libs import PirateBayAPI
 
 from urllib import url2pathname
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(TEMPLATE_PATH),
+    extensions=['jinja2.ext.autoescape'])
+
+def _render_template(template_name, args= {}):
+    template = JINJA_ENVIRONMENT.get_template(template_name)
+    return template.render(args)
+
 # GET /
-class RootHandler(webapp.RequestHandler):
+class RootHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write(template.render(TEMPLATE_PATH + 'index.html', {}))
+        self.response.out.write(_render_template('index.html'))
     def post(self):
         """POSTing on root I can redirect to a pretty url without having to add trouble javascript on the client side"""
         
@@ -38,7 +46,7 @@ class RootHandler(webapp.RequestHandler):
 # TORRENT SEARCH HANDLERS
 
 # GET /requestResultsForValue?value=spider+man&filter=video
-class RequestResultsForValueHandler(webapp.RequestHandler):
+class RequestResultsForValueHandler(webapp2.RequestHandler):
     def get(self, value, filter= 'none', orderby= 'SE'):
                 
          # Check if values have been passed, if not redirect to root
@@ -51,34 +59,45 @@ class RequestResultsForValueHandler(webapp.RequestHandler):
          # Render Response
          results = PirateBayAPI.requestResultsForValue(value=value, filter=filter, orderBy=orderby)
          base_url = self.request.host_url + '/s/%s/f/%s/' % (value, filter)
-         self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : "Results for: %s" % value, 'sortable' : True, 'base_url' : base_url}))
+         self.response.out.write(_render_template('search-results.html'), {
+             'results' : results,
+             'title' : "Results for: %s" % value,
+             'sortable' : True,
+             'base_url' : base_url
+         })
 
 # GET /requestResultsforTop100?filter=video
-class RequestResultsforTop100(webapp.RequestHandler):
+class RequestResultsforTop100(webapp2.RequestHandler):
     def get(self, filter= 'none'):
         results = PirateBayAPI.requestResultsforTop100(filter=filter)
         
         title = "Top 100"
         if filter != 'none': title += " in %s" % filter
         
-        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : title, 'sortable' : False}))
+        self.response.out.write(_render_template('search-results.html'), {
+             'results' : results,
+             'title' : title,
+             'sortable' : False
+        })
 
 # GET /requestResultsforRecentUploads
-class RequestResultsforRecentUploads(webapp.RequestHandler):
+class RequestResultsforRecentUploads(webapp2.RequestHandler):
     def get(self):
         results = PirateBayAPI.requestResultsforRecentUploads()
-        self.response.out.write(template.render(TEMPLATE_PATH + 'search-results.html', {'results' : results, 'title' : 'Recent uploads', 'sortable' : False}))
+        self.response.out.write(_render_template('search-results.html'), {
+            'results' : results,
+            'title' : 'Recent uploads',
+            'sortable' : False
+        })
 
 # END TORRENT SEARCH HANDLERS
-
-def main():
-    application = webapp.WSGIApplication([
-    ('/s/(.*)/f/(.*)/o/(.*)/', RequestResultsForValueHandler), ('/s/(.*)/f/(.*)/', RequestResultsForValueHandler), ('/s/(.*)/', RequestResultsForValueHandler),
-    ('/top/f/(.*)/', RequestResultsforTop100), ('/top/', RequestResultsforTop100),
-    ('/', RootHandler)],
-    debug=False)
-    util.run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-    main()
+app= webapp2.WSGIApplication([
+        ('/s/(.*)/f/(.*)/o/(.*)/', RequestResultsForValueHandler),
+        ('/s/(.*)/f/(.*)/', RequestResultsForValueHandler),
+        ('/s/(.*)/', RequestResultsForValueHandler),
+        ('/top/f/(.*)/', RequestResultsforTop100),
+        ('/top/', RequestResultsforTop100),
+        ('/', RootHandler)
+    ],
+    debug= 'GAE_DEBUG' in os.environ
+)
