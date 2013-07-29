28289006cbf00cc662c86fbaef6bc1f247ae1f22
Daniel Pyrathon <pirosb3@gmail.com>
2013-07-29 13:35:53 +0200

    added test cases

---
+++ tests.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import unittest
+
+
+class PirateBayTestCase(unittest.TestCase):
+
+    def test_top_100_work(self):
+        result = PirateBayAPI.requestResultsforTop100()
+        self.assertEqual(100, len(result))
+
+    def test_recent_word(self):
+        result = PirateBayAPI.requestResultsforRecentUploads()
+        self.assertEqual(30, len(result))
+
+
+
+if __name__ == '__main__':
+
+    # Fix GAE path
+    import dev_appserver
+    dev_appserver.fix_sys_path()
+
+    # Add TheItalianBay path
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, this_dir)
+    from libs import PirateBayAPI
+
+    # Import and initialize remote API
+    from google.appengine.ext.remote_api import remote_api_stub
+    remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', 
+        lambda: ('pirosb3@gmail.com', os.environ['TIB_PASSWORD']), 'theitalianbay.appspot.com')
+
+    # Disable caching
+    os.environ['DISABLE_CACHE'] = 'TRUE'
+
+    # Run tests
+    unittest.main()
